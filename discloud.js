3056eeb148bfc24be1e2ec96370a74433e162a85
eduardanasc2006-blip <eduardanasc2006@gmail.com>
2026-05-30 15:20:40 -0300

    Update discloud.js

--- discloud.js
+++ discloud.js
@@ -1,6 +1,6 @@
 process.env.PORT = process.env.PORT || "8080";
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
-process.env.DISCORD_BOT_TOKEN = MTUwOTE0NjkzMjQ3ODQ3NjM4OQ.GcVm_n.lKP3mr9Be_Gxj0Z-ZTNGW7wrIAkYwTdIkTJHDM; //
+process.env.DISCORD_BOT_TOKEN = "MTUwOTE0NjkzMjQ3ODQ3NjM4OQ.Gtxykf._qruY7tzgv2d4LPifM2LOKGuMeCukmYSvqQUqA"; //
 console.log("[FiskBot] Iniciando...");
 import("./artifacts/api-server/dist/index.mjs").catch((err) => {
   console.error("[FiskBot] Erro ao iniciar:", err);
