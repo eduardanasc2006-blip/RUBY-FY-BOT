833d752785bfec1c615a1e5a94f622dae8cf0235
eduardanasc2006-blip <eduardanasc2006@gmail.com>
2026-06-04 23:41:33 -0300

    Add files via upload

--- src/db/models/Missao.mjs
+++ src/db/models/Missao.mjs
@@ -2,4 +2,5 @@ import { makeModel } from '../sqlite.mjs';
 export default makeModel('missoes', {
   jsonFields: ['diarias', 'semanais'],
   dateFields: ['ultimaRenovacaoDiaria', 'ultimaRenovacaoSemanal'],
+  textFields: ['ultimaDiaMissao', 'ultimaSemanaMissao'],
 });
